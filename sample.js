d21ebfbe74cb90222f190ba4c6cf6e7ce150a087
MohanrajRKM <161418444+MohanrajRKM@users.noreply.github.com>
2024-11-26 12:26:06 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,297 @@
+var stringData = [
+    {
+        'OrderID': 10248,
+        'CustomerID': 'VINET',
+        'OrderDate': '1996-07-04T00:00:00.000Z',
+        'ShippedDate': '1996-07-16T00:00:00.000Z',
+        'Freight': 32.38,
+        'ShipName': 'Vins et alcools Chevalier',
+        'ShipAddress': '59 rue de l\'Abbaye',
+        'ShipCity': 'Reims',
+        'ShipRegion': null,
+        'ShipCountry': 'France'
+    },
+    {
+        'OrderID': 10249,
+        'CustomerID': 'TOMSP',
+        'OrderDate': '1996-07-05T00:00:00.000Z',
+        'ShippedDate': '1996-07-10T00:00:00.000Z',
+        'Freight': 11.61,
+        'ShipName': 'Toms SpezialitÃ¤ten',
+        'ShipAddress': 'Luisenstr. 48',
+        'ShipCity': 'MÃ¼nster',
+        'ShipRegion': null,
+        'ShipCountry': 'Germany'
+    },
+    {
+        'OrderID': 10250,
+        'CustomerID': 'HANAR',
+        'OrderDate': '1996-07-08T00:00:00.000Z',
+        'ShippedDate': '1996-07-12T00:00:00.000Z',
+        'Freight': 65.83,
+        'ShipName': 'Hanari Carnes',
+        'ShipAddress': 'Rua do PaÃ§o, 67',
+        'ShipCity': 'Rio de Janeiro',
+        'ShipRegion': 'RJ',
+        'ShipCountry': 'Brazil'
+    },
+    {
+        'OrderID': 10251,
+        'CustomerID': 'VICTE',
+        'OrderDate': '1996-07-08T00:00:00.000Z',
+        'ShippedDate': '1996-07-15T00:00:00.000Z',
+        'Freight': 41.34,
+        'ShipName': 'Victuailles en stock',
+        'ShipAddress': '2, rue du Commerce',
+        'ShipCity': 'Lyon',
+        'ShipRegion': null,
+        'ShipCountry': 'France'
+    },
+    {
+        'OrderID': 10252,
+        'CustomerID': 'SUPRD',
+        'OrderDate': '1996-07-09T00:00:00.000Z',
+        'ShippedDate': '1996-07-11T00:00:00.000Z',
+        'Freight': 51.3,
+        'ShipName': 'SuprÃªmes dÃ©lices',
+        'ShipAddress': 'Boulevard Tirou, 255',
+        'ShipCity': 'Charleroi',
+        'ShipRegion': null,
+        'ShipCountry': 'Belgium'
+    },
+    {
+        'OrderID': 10253,
+        'CustomerID': 'HANAR',
+        'OrderDate': '1996-07-10T00:00:00.000Z',
+        'ShippedDate': '1996-07-16T00:00:00.000Z',
+        'Freight': 58.17,
+        'ShipName': 'Hanari Carnes',
+        'ShipAddress': 'Rua do PaÃ§o, 67',
+        'ShipCity': 'Rio de Janeiro',
+        'ShipRegion': 'RJ',
+        'ShipCountry': 'Brazil'
+    },
+    {
+        'OrderID': 10254,
+        'CustomerID': 'CHOPS',
+        'OrderDate': '1996-07-11T00:00:00.000Z',
+        'ShippedDate': '1996-07-23T00:00:00.000Z',
+        'Freight': 22.98,
+        'ShipName': 'Chop-suey Chinese',
+        'ShipAddress': 'Hauptstr. 31',
+        'ShipCity': 'Bern',
+        'ShipRegion': null,
+        'ShipCountry': 'Switzerland'
+    },
+    {
+        'OrderID': 10255,
+        'CustomerID': 'RICSU',
+        'OrderDate': '1996-07-12T00:00:00.000Z',
+        'ShippedDate': '1996-07-15T00:00:00.000Z',
+        'Freight': 148.33,
+        'ShipName': 'Richter Supermarkt',
+        'ShipAddress': 'Starenweg 5',
+        'ShipCity': 'GenÃ¨ve',
+        'ShipRegion': null,
+        'ShipCountry': 'Switzerland'
+    },
+    {
+        'OrderID': 10256,
+        'CustomerID': 'WELLI',
+        'OrderDate': '1996-07-15T00:00:00.000Z',
+        'ShippedDate': '1996-07-17T00:00:00.000Z',
+        'Freight': 13.97,
+        'ShipName': 'Wellington Importadora',
+        'ShipAddress': 'Rua do Mercado, 12',
+        'ShipCity': 'Resende',
+        'ShipRegion': 'SP',
+        'ShipCountry': 'Brazil'
+    },
+    {
+        'OrderID': 10257,
+        'CustomerID': 'HILAA',
+        'OrderDate': '1996-07-16T00:00:00.000Z',
+        'ShippedDate': '1996-07-22T00:00:00.000Z',
+        'Freight': 81.91,
+        'ShipName': 'HILARION-Abastos',
+        'ShipAddress': 'Carrera 22 con Ave. Carlos Soublette #8-35',
+        'ShipCity': 'San CristÃ³bal',
+        'ShipRegion': 'TÃ¡chira',
+        'ShipCountry': 'Venezuela'
+    },
+    {
+        'OrderID': 10258,
+        'CustomerID': 'ERNSH',
+        'OrderDate': '1996-07-17T00:00:00.000Z',
+        'ShippedDate': '1996-07-23T00:00:00.000Z',
+        'Freight': 140.51,
+        'ShipName': 'Ernst Handel',
+        'ShipAddress': 'Kirchgasse 6',
+        'ShipCity': 'Graz',
+        'ShipRegion': null,
+        'ShipCountry': 'Austria'
+    },
+    {
+        'OrderID': 10259,
+        'CustomerID': 'CENTC',
+        'OrderDate': '1996-07-18T00:00:00.000Z',
+        'ShippedDate': '1996-07-25T00:00:00.000Z',
+        'Freight': 3.25,
+        'ShipName': 'Centro comercial Moctezuma',
+        'ShipAddress': 'Sierras de Granada 9993',
+        'ShipCity': 'MÃ©xico D.F.',
+        'ShipRegion': null,
+        'ShipCountry': 'Mexico'
+    },
+    {
+        'OrderID': 10260,
+        'CustomerID': 'OTTIK',
+        'OrderDate': '1996-07-19T00:00:00.000Z',
+        'ShippedDate': '1996-07-29T00:00:00.000Z',
+        'Freight': 55.09,
+        'ShipName': 'Ottilies KÃ¤seladen',
+        'ShipAddress': 'Mehrheimerstr. 369',
+        'ShipCity': 'KÃ¶ln',
+        'ShipRegion': null,
+        'ShipCountry': 'Germany'
+    },
+]
+
+var dropdown = new ej.dropdowns.DropDownList({
+    dataSource: ["Pagination", "Virtual Scroll", "Infinite Scroll"],
+    change: ddChange,
+    value: "Pagination",
+    width: 250,
+})
+dropdown.appendTo('#ddlelement');
+
+var grid = new ej.grids.Grid({
+    dataSource: stringData,
+    height: 270,
+    allowPaging: true,
+    allowSorting: true,
+    allowGrouping: true,
+    allowResizing: true,
+    allowFiltering: true,
+    allowPdfExport: true,
+    allowReordering: true,
+    allowExcelExport: true,
+    showColumnChooser: true,
+    allowRowDragAndDrop: true,
+    groupSettings: { enableLazyLoading: true },
+    filterSettings: { type: 'Excel', enableInfiniteScrolling: true, },
+    pageSettings: { pageCount: 2, pageSizes: true },
+    editSettings: { allowAdding: true, allowEditing: true, allowDeleting: true, mode: 'Normal' },
+    toolbar: ['Add', 'Delete', 'Update', 'Cancel', 'Search', 'ExcelExport', 'PdfExport', 'CsvExport', 'Print', 'ColumnChooser'],
+    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
+        'Copy', 'Edit', 'Delete', 'Save', 'Cancel', 'PdfExport', 'ExcelExport',
+        'CsvExport', 'FirstPage', 'PrevPage', 'LastPage', 'NextPage'],
+    columns: [
+        { field: 'OrderID', headerText: 'Order ID', isPrimaryKey: true, textAlign: 'Right', width: 120, validationRules: { required: true }},
+        { field: 'CustomerID', headerText: 'Customer ID', width: 120, editType: 'dropdownedit',  },
+        { field: 'Freight', headerText: 'Frieght', width: 120, format: 'C2', editType: 'numericedit', textAlign: 'Right' },
+        { field: 'OrderDate', headerText: 'Order Date', width: 120, format: 'yMd', editType: 'datepickeredit', textAlign: 'Right' },
+        { field: 'ShipCity', headerText: 'Ship City', width: 120, validationRules: { required: true, minLength: 3, maxLength: 20 } }
+    ],
+    aggregates: [{
+        columns: [{
+            type: 'Sum',
+            field: 'Freight',
+            format: 'C2',
+            footerTemplate: 'Sum: ${Sum}'
+        }]
+    },
+    {
+        columns: [{
+            type: 'Average',
+            field: 'Freight',
+            format: 'C2',
+            footerTemplate: 'Average: ${Average}'
+        }]
+    }],
+});
+grid.appendTo('#Grid');
+
+    var grouping = new ej.buttons.CheckBox();
+    grouping.appendTo('#columnMenu');
+    document.getElementById('columnMenu').onclick = function () {
+        if (grouping.checked) {
+            grid.showColumnMenu = true;
+        }
+        else {
+            grid.showColumnMenu = false;
+        }
+    };
+
+    var rtl = new ej.buttons.CheckBox();
+    rtl.appendTo('#rtl');
+    document.getElementById('rtl').onclick = function () {
+        if (rtl.checked) {
+            grid.enableRtl = true;
+        }
+        else {
+            grid.enableRtl = false;
+        }
+    };
+
+    var frozen = new ej.buttons.CheckBox();
+    frozen.appendTo('#frozen');
+    document.getElementById('frozen').onclick = function () {
+        if (frozen.checked) {
+            grid.frozenColumns = 1;
+            grid.frozenRows = 3;
+        }
+        else {
+            grid.frozenColumns = 0;
+            grid.frozenRows = 0;
+        }
+    };
+
+    var checkBoxObj = new ej.buttons.CheckBox({ change: onBiggerChange });
+        checkBoxObj.appendTo('#bigger');
+
+    function onBiggerChange(e) {
+        e.checked ? document.body.classList.add('e-bigger') : document.body.classList.remove('e-bigger');
+    }
+
+    function ddChange(args) {
+        grid.columns.map(x=> x.visible = true);
+        if (args.value === "Pagination") {
+            grid.setProperties({
+                groupSettings: { columns: [] },
+                filterSettings: { columns: [] },
+                sortSettings: { columns: [] },
+                allowPaging: true,
+                enableVirtualization: false,
+                enableInfiniteScrolling: false,
+            }, true)
+            // grid.clearGrouping();
+        }
+        if (args.value === "Virtual Scroll") {
+            grid.setProperties({
+                groupSettings: { columns: [] },
+                filterSettings: { columns: [] },
+                sortSettings: { columns: [] },
+                allowPaging: false,
+                enableVirtualization: true,
+                enableInfiniteScrolling: false,
+            }, true)
+            // grid.clearGrouping();
+            grid.gridPager = null;
+        }
+        if (args.value === "Infinite Scroll") {
+            grid.setProperties({
+                groupSettings: { columns: [] },
+                filterSettings: { columns: [] },
+                sortSettings: { columns: [] },
+                allowPaging: false,
+                enableVirtualization: false,
+                enableInfiniteScrolling: true,
+            }, true)
+            // grid.clearGrouping();
+            grid.gridPager = null;
+        }
+        grid.freezeRefresh();
+    }
+
+  
